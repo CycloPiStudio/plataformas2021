89c6d4a50d8d1c66236a24a4b9d5a8ac656ce340
brecabre <bravomantonio@gmail.com>
2021-12-05 19:37:22 +0100

    subo jugador

--- plataformas_2021/jugador/jugador.gd
+++ plataformas_2021/jugador/jugador.gd
@@ -1,22 +1,26 @@
+
 extends KinematicBody2D
 
-const GRAVITY = 200.0
-const WALK_SPEED = 200
-
+var speed = 250
 var velocity = Vector2()
 
+func get_input():
+	# Detect up/down/left/right keystate and only move when pressed.
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
 func _physics_process(delta):
-	velocity.y += delta * GRAVITY
+	get_input()
+	move_and_collide(velocity * delta)
 
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -WALK_SPEED
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x =  WALK_SPEED
-	else:
-		velocity.x = 0
 
-	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
-
-	# The second parameter of "move_and_slide" is the normal pointing up.
-	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
-	move_and_slide(velocity, Vector2(0, -1))
+func _on_Area2D_body_entered(body):
+	print(body)
