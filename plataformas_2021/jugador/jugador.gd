49869f0281d533d48f1268e217e5e84d6a4a642e
brecabre <bravomantonio@gmail.com>
2021-12-05 19:55:34 +0100

    Update jugador.gd

--- plataformas_2021/jugador/jugador.gd
+++ plataformas_2021/jugador/jugador.gd
@@ -1,25 +1,32 @@
-
 extends KinematicBody2D
 
-var speed = 250
+export (int) var run_speed = 100
+export (int) var jump_speed = -400
+export (int) var gravity = 1200
+
 var velocity = Vector2()
+var jumping = false
 
 func get_input():
-	# Detect up/down/left/right keystate and only move when pressed.
-	velocity = Vector2()
-	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('ui_down'):
-		velocity.y += 1
-	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
+    velocity.x = 0
+    var right = Input.is_action_pressed('ui_right')
+    var left = Input.is_action_pressed('ui_left')
+    var jump = Input.is_action_just_pressed('ui_select')
+
+    if jump and is_on_floor():
+        jumping = true
+        velocity.y = jump_speed
+    if right:
+        velocity.x += run_speed
+    if left:
+        velocity.x -= run_speed
 
 func _physics_process(delta):
-	get_input()
-	move_and_collide(velocity * delta)
+    get_input()
+    velocity.y += gravity * delta
+    if jumping and is_on_floor():
+        jumping = false
+    velocity = move_and_slide(velocity, Vector2(0, -1))
 
 
 func _on_Area2D_body_entered(body):
