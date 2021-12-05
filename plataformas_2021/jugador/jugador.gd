3e08a681ce5ee7cda8e8ace28d6fdcfd85124898
brecabre <bravomantonio@gmail.com>
2021-12-05 19:46:08 +0100

    Update jugador.gd

--- plataformas_2021/jugador/jugador.gd
+++ plataformas_2021/jugador/jugador.gd
@@ -1,26 +1,39 @@
-
 extends KinematicBody2D
 
-var speed = 250
+const GRAVITY = 200.0
+const WALK_SPEED = 200
+var direccion 
 var velocity = Vector2()
-
-func get_input():
-	# Detect up/down/left/right keystate and only move when pressed.
-	velocity = Vector2()
-	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('ui_down'):
-		velocity.y += 1
-	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
-
+func _ready():
+	direccion = "left"
 func _physics_process(delta):
-	get_input()
-	move_and_collide(velocity * delta)
+	velocity.y += delta * GRAVITY
 
+	if direccion == "left":
+		velocity.x = -WALK_SPEED
+	elif direccion =="right":
+		velocity.x =  WALK_SPEED
+	else:
+		velocity.x = 0
+	if $RayIzquierda.is_colliding() == false :
+		direccion = "right"
+	elif $RayDerecha.is_colliding() == false :
+		direccion = "left"
+	else:
+		pass
+	animacion(direccion)
 
-func _on_Area2D_body_entered(body):
-	print(body)
+func animacion(di):
+	if  di == "left":
+		$AnimationPlayer.play("andar_izquierda")
+	elif  di == "right":
+		$AnimationPlayer.play("andar_derecha")
+	else :
+		pass
+			
+	
+	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
+
+	# The second parameter of "move_and_slide" is the normal pointing up.
+	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
+	move_and_slide(velocity, Vector2(0, -1))
