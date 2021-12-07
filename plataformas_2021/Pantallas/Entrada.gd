0e4117e5f5c173b8688508e019e1f54f5fde928c
CycloPi <venti3d@gmail.com>
2021-12-07 22:36:10 +0100

    temporizador, más cambio de pantalla

--- plataformas_2021/Pantallas/Entrada.gd
+++ plataformas_2021/Pantallas/Entrada.gd
@@ -1,22 +1,14 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
 var cont = 0
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-
 	cont += 1
+	$animaentrada/Timer.set_wait_time(10.3)
 	$animaentrada/Timer.start()
 	pass #
 	
-
-
 func _on_Timer_timeout():
-	
 	if cont == 1:
 		$animaentrada/Audio1.play()
 		$animaentrada/Sprite0.show()
@@ -24,7 +16,6 @@ func _on_Timer_timeout():
 		cont += 1
 	elif cont ==2:
 		$animaentrada/Audio2.play()
-		
 		cont += 1
 	elif cont ==3:
 		$animaentrada/Audio3.play()
@@ -38,21 +29,20 @@ func _on_Timer_timeout():
 		$animaentrada/Audio5.play()
 		$animaentrada/Sprite5.show()
 		cont += 1
-
+	elif cont ==6:
+		print("cambiar pantalla", get_tree().change_scene("res://Pantallas/Pantalla1.tscn"))
 	print(cont)
 	pass # Replace with function body.
 
-
 func _on_Audio1_finished():
+	$animaentrada/Timer.set_wait_time(1)
 	$animaentrada/Timer.start()
 	pass # Replace with function body.
-
 
 func _on_Audio2_finished():
 	$animaentrada/Timer.start()
 	$animaentrada/Sprite1.hide()
 	pass # Replace with function body.
-
 
 func _on_Audio3_finished():
 	$animaentrada/Timer.start()
