0b449ab785f5b1bef387dc9ea73d7de242644d34
brecabre <bravomantonio@gmail.com>
2021-12-07 21:47:55 +0100

    SUBO_JUGADOR

--- plataformas_2021/jugador/jugador.gd
+++ plataformas_2021/jugador/jugador.gd
@@ -8,27 +8,32 @@ var velocity = Vector2()
 var jumping = false
 
 func get_input():
-    velocity.x = 0
-    var right = Input.is_action_pressed('ui_right')
-    var left = Input.is_action_pressed('ui_left')
-    var jump = Input.is_action_just_pressed('ui_select')
+	velocity.x = 0
+	var right = Input.is_action_pressed('ui_right')
+	var left = Input.is_action_pressed('ui_left')
+	var jump = Input.is_action_just_pressed('ui_select')
 
-    if jump and is_on_floor():
-        jumping = true
-        velocity.y = jump_speed
-    if right:
-        velocity.x += run_speed
-    if left:
-        velocity.x -= run_speed
-
+	if jump and is_on_floor():
+		jumping = true
+		velocity.y = jump_speed
+	
+	if right:
+		velocity.x += run_speed
+		
+	if left:
+		velocity.x -= run_speed
+		
 func _physics_process(delta):
-    get_input()
-    velocity.y += gravity * delta
-    if jumping and is_on_floor():
-        jumping = false
-    velocity = move_and_slide(velocity, Vector2(0, -1))
+	get_input()
+	velocity.y += gravity * delta
+	if jumping and is_on_floor():
+		jumping = false
+	velocity = move_and_slide(velocity, Vector2(0, -1))
 
-
+#func animacion(r,l):
+#	if r :
+#		$AnimationPlayer.play("caminar_derecha")
+#	elif l:
+#		$AnimationPlayer.play("caminar_izquierda")
 func _on_Area2D_body_entered(body):
 	print(body)
-
